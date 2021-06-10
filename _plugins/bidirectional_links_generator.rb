33a0eedf7cd0f291565b681f011c4ccc9633e0e5
francescoimola <frn.imola@gmail.com>
2021-06-10 16:37:11 +0100

    Revert "Stop replacing dashes in link generator"
    This reverts commit f168d65c8b1fffd2cbba592fb682169f485054a7.

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -18,7 +18,7 @@ class BidirectionalLinksGenerator < Jekyll::Generator
         title_from_filename = File.basename(
           note_potentially_linked_to.basename,
           File.extname(note_potentially_linked_to.basename)
-        ).gsub('_', ' ').capitalize
+        ).gsub('_', ' ').gsub('-', ' ').capitalize
 
         # Replace double-bracketed links with label using note title
         # [[A note about cats|this is a link to the note about cats]]

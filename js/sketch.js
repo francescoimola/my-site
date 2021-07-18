4d0440b1a9afa567436e5acb17958135890fcc9c
francescoimola <frn.imola@gmail.com>
2021-07-18 22:04:29 +0100

    Fixed favicons console error and shortened JS code

--- js/sketch.js
+++ js/sketch.js
@@ -13,14 +13,14 @@ function touchMoved() {
 
 function draw() {
     if (mouseIsPressed) {
-        hatching()
+        hatching(16, 86, 125, 50, 1, 3)
     }
 }
 
-function hatching() {
+function hatching(r, g, b, alpha, strokeweight, lerptimes) {
     // set the color and brush style
-    stroke(16, 86, 125, 50);
-    strokeWeight(1);
+    stroke(r, g, b, alpha);
+    strokeWeight(strokeweight);
 
     // calculate the speed of the mouse
     let speed = abs(mouseX - pmouseX) + abs(mouseY - pmouseY)
@@ -31,15 +31,12 @@ function hatching() {
     // set the vector magnitude (the line length) based on the mouse speed
     vector.setMag(speed / 2)
 
-    // set the number of times we lerp the line
-    const lerps = 3
-
     // repeat the line with lerping
-    for (let i = 0; i < lerps; i++) {
+    for (let i = 0; i < lerptimes; i++) {
 
         // find the lerped X and Y coordinates
-        const x = lerp(mouseX, pmouseX, i / lerps)
-        const y = lerp(mouseY, pmouseY, i / lerps)
+        const x = lerp(mouseX, pmouseX, i / lerptimes)
+        const y = lerp(mouseY, pmouseY, i / lerptimes)
 
         // draw a line
         line(x - vector.x, y - vector.y, x + vector.x, y + vector.y)

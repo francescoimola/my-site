cf7b286bea42bd731d0246be414a3dcdd330d01c
Francesco Imola <frn.imola@gmail.com>
2022-12-26 20:50:03 +0000

    Trying to dynamically load content

--- showhide.js
+++ showhide.js
@@ -1,3 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+	const navigation = document.getElementsByClassName("selectors")[0];
+	const btns = navigation.getElementsByTagName("button"); 
+
+// Loop through the buttons and add the active class to the current/clicked button
+	for (var i = 0; i < btns.length; i++) {
+		btns[i].addEventListener("click", function() {
+			var current = document.getElementsByClassName("highlighted");
+
+    // If there's no active class
+			if (current.length > 0) {
+				current[0].className = current[0].className.replace("highlighted", "");
+			}
+
+    // Add the active class to the current/clicked button
+			this.className += "highlighted";
+		});
+	}
+
+} );
+
+//target all buttons elements with data-href property
+$('button[data-href]').click(function () {
+	//load the contents of the url specified by the href into addnewplayer
+	$(".myart").load($(this).data('href'));
+})
+
+
+/* 
 let Buttons = document.querySelectorAll(".selectors button");
 
 for (let button of Buttons) {
@@ -15,3 +44,4 @@ for (let button of Buttons) {
 		}
 	});
 }
+*/

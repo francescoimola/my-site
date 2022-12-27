52ceac0daa726b5bf368b3dbcebc5f47e8170ecc
Francesco Imola <frn.imola@gmail.com>
2022-12-27 03:03:27 +0100

    Looking great, more work necessary though

--- showhide.js
+++ showhide.js
@@ -1,47 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+	$("body").removeClass("preload");
+	
 	const navigation = document.getElementsByClassName("selectors")[0];
-	const btns = navigation.getElementsByTagName("button"); 
+	const btns = navigation.getElementsByTagName("button");
 
-// Loop through the buttons and add the active class to the current/clicked button
-	for (var i = 0; i < btns.length; i++) {
-		btns[i].addEventListener("click", function() {
-			var current = document.getElementsByClassName("highlighted");
+	// Loop through the buttons and add the active class to the current/clicked button
+	for (let i = 0; i < btns.length; i++) {
+		btns[i].addEventListener("click", function () {
+			let current = document.getElementsByClassName("highlighted");
+			let mylink = $(this).data("href");
 
-    // If there's no active class
+			// If there's no active class
 			if (current.length > 0) {
+				$(".category").empty();
 				current[0].className = current[0].className.replace("highlighted", "");
 			}
 
-    // Add the active class to the current/clicked button
+			// Load corresponding html file, as stated in the data-href of that button
+			$(".category").load(mylink);
+
+			// Add the active class to the current/clicked button
 			this.className += "highlighted";
 		});
 	}
 
-} );
-
-//target all buttons elements with data-href property
-$('button[data-href]').click(function () {
-	//load the contents of the url specified by the href into addnewplayer
-	$(".myart").load($(this).data('href'));
-})
-
-
-/* 
-let Buttons = document.querySelectorAll(".selectors button");
-
-for (let button of Buttons) {
-	button.addEventListener("click", (e) => {
-
-		let allContent = document.querySelectorAll(".category");
-		for (let category of allContent) {
-			if (category.getAttribute("data-number") === button.getAttribute("data-number")) {
-				category.style.display = "block";
-				category.style.visibility = "visible";
-			} else {
-				category.style.display = "none";
-				category.style.visibility = "hidden";
-			}
-		}
-	});
-}
-*/
+});
